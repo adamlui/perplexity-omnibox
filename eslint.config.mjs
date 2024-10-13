35d5b435e16e93355093cde8b55165b3a42253b2
Adam Lui 刘展鹏 <adamlui@protonmail.com>
2024-10-12 23:21:34 -0700

    Changed `languageOptions.ecmaVersion` to `latest`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,7 @@ export default [
             'no-useless-escape': 'off', // allow all escape chars cause ESLint sucks at detecting truly useless ones
             'no-unused-vars': ['error', { 'caughtErrors': 'none' }] // allow unused named args in catch blocks
         },
-        languageOptions: { ecmaVersion: 2022, sourceType: 'script', globals: { chrome: 'readonly' }}
+        languageOptions: { ecmaVersion: 'latest', sourceType: 'script', globals: { chrome: 'readonly' }}
     },
     { files: ['**/*.mjs'], languageOptions: { sourceType: 'module' }},
     { files: ['**/*.json'], ...json.configs['recommended'] }
