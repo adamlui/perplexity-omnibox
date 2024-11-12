7c535b3c2876602e185d09d3d8f0c85cb39c2a32
adamlui@protonmail.com <adamlui@protonmail.com>
2024-11-12 12:19:24 -0800

    Added @eslint/markdown as dev dependency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import json from '@eslint/json'
+import markdown from '@eslint/markdown'
 
 export default [
     {
@@ -18,5 +19,13 @@ export default [
         languageOptions: { ecmaVersion: 'latest', sourceType: 'script', globals: { chrome: 'readonly' }}
     },
     { files: ['**/*.mjs'], languageOptions: { sourceType: 'module' }},
-    { files: ['**/*.json'], ignores: ['**/package-lock.json'], language: 'json/json', ...json.configs.recommended }
+    { files: ['**/*.json'], ignores: ['**/package-lock.json'], language: 'json/json', ...json.configs.recommended },
+    {
+        files: ['**/*.md'], language: 'markdown/commonmark', plugins: { markdown },
+        rules: {
+            ...markdown.configs.recommended[0].rules,
+            'markdown/heading-increment': 'off', // allow headings to skip levels
+            'markdown/fenced-code-language': 'off' // allow code blocks w/ no language specified
+        }
+    }
 ]
